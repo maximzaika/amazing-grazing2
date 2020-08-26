ac1d693bfee5cb2523b28b12103729e3e23437ae
Maxim Zaika <maximzaika@hotmail.com>
2020-08-26 14:57:31 +0800

    update 2:57pm

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- Server side files -->
+<?php 
+	require_once "server_config.php";
+	require_once "php/gnewsAPI.php";
+	
+	$testquery = "SELECT testID, firstpage_info FROM test";
+	$test_result = $con -> query($testquery);
+	
+	$final;
+	if ($test_result->num_rows > 0) {
+		while($row = $test_result->fetch_assoc()) {
+			$post_firstpage_result = $row['firstpage_info'];
+			$post_id_result = $row['testID'];
+
+			$final =  $final .'<h1>' . $post_firstpage_result . '</h1>';
+		}
+	}
+	
+	$con -> close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<body>
+		<h1><?php echo htmlspecialchars_decode($final);?>
+	</body>
+</html>
