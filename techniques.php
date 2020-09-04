c2c3a776eeb7ed2d1534ccfe5f8cbb72d5eed399
Chongyang <cliu0040@student.monash.edu>
2020-09-04 22:57:44 +0800

    add seasonal grazing gif

--- techniques.php
+++ techniques.php
@@ -162,11 +162,11 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
 													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8" style="margin: auto;">
-													<h5>The most appropriate approach that protects Australian grasslands!</h5>
+												<div class="col-xxs-8 col-xs-8 col-sm-11 col-md-10 col-lg-10" style="margin: auto;">
+													<h5>Most simple, popular, and low effort on labour</h5>
 												</div>
 											</div>
 										</div>
@@ -177,10 +177,10 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
 													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
 													<h5>Temporary fencing is used</h5>
 												</div>
 											</div>
@@ -191,10 +191,10 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
-													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
+													<i class="fa fa-times fa-times-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
 													<h5>Damages soil, stream banks, and native vegetation</h5>
 												</div>
 											</div>
@@ -205,10 +205,10 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
-													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
+													<i class="fa fa-times fa-times-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
 													<h5>Low productivity and unsustainable development in the long-term</h5>
 												</div>
 											</div>
@@ -252,7 +252,7 @@
 						<div class="row">
 							
 							<div class="col-xxs-12 col-xs-12 col-sm-12 col-md-12 col-lg-6" style="margin:auto;">
-								<img src="images/rotational-grazing.gif" class="animated-gif" alt="SEASONAL GRAZING">
+								<img src="images/seasonal-grazing.gif" class="animated-gif" alt="SEASONAL GRAZING">
 							</div>
 							
 							<div class="col-xxs-12 col-sx-12 col-sm-12 col-md-12 col-lg-6" style="margin:auto;">
@@ -261,11 +261,11 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
-													<i class="fa fa-times fa-times-techniques" aria-hidden="true"></i>
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
+													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8" style="margin: auto;">
-													<h5>Spring</h5>
+												<div class="col-xxs-8 col-xs-8 col-sm-11 col-md-10 col-lg-10" style="margin: auto;">
+													<h5>Most simple, popular, and low effort on labour</h5>
 												</div>
 											</div>
 										</div>
@@ -276,11 +276,11 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
-													<i class="fa fa-times fa-times-techniques" aria-hidden="true"></i>
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
+													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
-													<h5>Summer</h5>
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
+													<h5>Temporary fencing is used</h5>
 												</div>
 											</div>
 										</div>
@@ -290,11 +290,11 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
-													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
+													<i class="fa fa-times fa-times-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
-													<h5>Autumn</h5>
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
+													<h5>Damages soil, stream banks, and native vegetation</h5>
 												</div>
 											</div>
 										</div>
@@ -304,11 +304,11 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
 													<i class="fa fa-times fa-times-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
-													<h5>Winter</h5>
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
+													<h5>Low productivity and unsustainable development in the long-term</h5>
 												</div>
 											</div>
 										</div>
@@ -359,11 +359,11 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
 													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8" style="margin: auto;">
-													<h5>The most appropriate approach that protects Australian grasslands!</h5>
+												<div class="col-xxs-8 col-xs-8 col-sm-11 col-md-10 col-lg-10" style="margin: auto;">
+													<h5>Most simple, popular, and low effort on labour</h5>
 												</div>
 											</div>
 										</div>
@@ -374,10 +374,10 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
 													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
 													<h5>Temporary fencing is used</h5>
 												</div>
 											</div>
@@ -388,10 +388,10 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
-													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
+													<i class="fa fa-times fa-times-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
 													<h5>Damages soil, stream banks, and native vegetation</h5>
 												</div>
 											</div>
@@ -402,10 +402,10 @@
 									<li class="service-techniques services-color">
 										<div class="container">
 											<div class="row">
-												<div class="col-md-4 text-center" style="margin: auto;">
-													<i class="fa fa-check fa-check-techniques" aria-hidden="true"></i>
+												<div class="col-xxs-4 col-xs-4 col-sm-1 col-md-2 col-lg-2 text-center" style="margin: auto;">
+													<i class="fa fa-times fa-times-techniques" aria-hidden="true"></i>
 												</div>
-												<div class="col-md-8 align-center" style="margin: auto">
+												<div class="col-xxs-8 col-xs-4 col-sm-11 col-md-10 col-lg-10" style="margin: auto">
 													<h5>Low productivity and unsustainable development in the long-term</h5>
 												</div>
 											</div>
