c4e34bf6c674486f5bec2af06775c3a0ea129064
Maxim Zaika <maximzaika@hotmail.com>
2020-09-06 13:16:39 +0800

    removed unnecessary files

---
+++ index_copy.php
@@ -0,0 +1,15 @@
+<!-- Server side files -->
+<?php 
+	require_once "server_config.php";
+	require_once "php/retrieve_techniques.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+	<?php 	
+		#echo retrieveTechniquePage($con, "SEASONAL GRAZING", "Carousel");
+		#echo retrieveTechniquePage($con, "ROTATIONAL GRAZING", "Methods");
+		#echo retrieveTechniquePage($con, "PATCH-BURN GRAZING", "No");
+    ?>
+</html>
