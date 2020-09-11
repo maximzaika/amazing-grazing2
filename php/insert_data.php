46c6e02513cccf6cefe4c3fab430ecb03b680917
Chongyang <cliu0040@student.monash.edu>
2020-09-11 13:18:23 +0800

    create livestock_numbers table and insert data

---
+++ php/insert_data.php
@@ -0,0 +1,283 @@
+<?php
+// sql to insert data
+$year=1973;
+$category1='Beef cattle';
+$category2='Dairy cattle';
+$category3='Sheep';
+//while (year<=2016){
+	$sql="insert into Livestock_Numbers (year,category,numbers)
+	values(1973,'".$category1."',26.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1974,'".$category1."',28.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1975,'".$category1."',29.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1976,'".$category1."',28.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1977,'".$category1."',26.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1978,'".$category1."',24.2);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1979,'".$category1."',23.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1980,'".$category1."',22.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1981,'".$category1."',21.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1982,'".$category1."',19.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1983,'".$category1."',19.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1984,'".$category1."',20.0);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1985,'".$category1."',20.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1986,'".$category1."',19.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1987,'".$category1."',19.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1988,'".$category1."',19.9);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1989,'".$category1."',20.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1990,'".$category1."',21.2);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1991,'".$category1."',21.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1992,'".$category1."',21.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1993,'".$category1."',23.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1994,'".$category1."',23.0);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1995,'".$category1."',23.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1996,'".$category1."',23.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1997,'".$category1."',23.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1998,'".$category1."',23.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1999,'".$category1."',24.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2000,'".$category1."',24.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2001,'".$category1."',24.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2002,'".$category1."',23.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2003,'".$category1."',24.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2004,'".$category1."',24.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2005,'".$category1."',25.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2006,'".$category1."',25.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2007,'".$category1."',24.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2008,'".$category1."',25.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2009,'".$category1."',24.0);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2010,'".$category1."',25.9);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2011,'".$category1."',25.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2012,'".$category1."',26.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2013,'".$category1."',26.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2014,'".$category1."',24.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2015,'".$category1."',22.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2016,'".$category1."',23.1);";
+	
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1973,'".$category2."',3.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1974,'".$category2."',3.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1975,'".$category2."',3.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1976,'".$category2."',3.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1977,'".$category2."',3.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1978,'".$category2."',2.9);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1979,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1980,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1981,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1982,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1983,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1984,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1985,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1986,'".$category2."',2.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1987,'".$category2."',2.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1988,'".$category2."',2.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1989,'".$category2."',2.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1990,'".$category2."',2.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1991,'".$category2."',2.4);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1992,'".$category2."',2.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1993,'".$category2."',2.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1994,'".$category2."',2.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1995,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1996,'".$category2."',3.0);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1997,'".$category2."',3.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1998,'".$category2."',3.2);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1999,'".$category2."',3.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2000,'".$category2."',3.2);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2001,'".$category2."',3.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2002,'".$category2."',3.0);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2003,'".$category2."',3.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2004,'".$category2."',3.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2005,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2006,'".$category2."',2.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2007,'".$category2."',2.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2008,'".$category2."',2.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2009,'".$category2."',2.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2010,'".$category2."',2.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2011,'".$category2."',2.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2012,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2013,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2014,'".$category2."',2.8);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2015,'".$category2."',2.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2016,'".$category2."',2.6);";
+	
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1973,'".$category3."',145);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1974,'".$category3."',152);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1975,'".$category3."',148);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1976,'".$category3."',135);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1977,'".$category3."',131);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1978,'".$category3."',134);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1979,'".$category3."',136);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1980,'".$category3."',134);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1981,'".$category3."',138);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1982,'".$category3."',131);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1983,'".$category3."',135);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1984,'".$category3."',146);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1985,'".$category3."',150);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1986,'".$category3."',149);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1987,'".$category3."',152);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1988,'".$category3."',162);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1989,'".$category3."',170);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1990,'".$category3."',163);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1991,'".$category3."',148);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1992,'".$category3."',138);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1993,'".$category3."',133);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1994,'".$category3."',121);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1995,'".$category3."',121);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1996,'".$category3."',120);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1997,'".$category3."',117);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1998,'".$category3."',115);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(1999,'".$category3."',119);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2000,'".$category3."',111);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2001,'".$category3."',106);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2002,'".$category3."',99.3);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2003,'".$category3."',101);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2004,'".$category3."',101);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2005,'".$category3."',91);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2006,'".$category3."',85.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2007,'".$category3."',76.9);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2008,'".$category3."',72.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2009,'".$category3."',68.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2010,'".$category3."',73.1);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2011,'".$category3."',74.7);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2012,'".$category3."',75.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2013,'".$category3."',72.6);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2014,'".$category3."',70.9);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2015,'".$category3."',67.5);";
+	$sql.="insert into Livestock_Numbers (year,category,numbers)
+	values(2016,'".$category3."',70.4);";
+
+
+	
+if ($con -> multi_query($sql) === True) {
+  echo "Insert data successfully";
+} else {
+	echo "Error inserting data: ";
+}
+
+?>
