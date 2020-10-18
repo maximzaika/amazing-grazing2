7e575846267f49cefda9efbd0081cd71c2cd16a5
Maxim Zaika <maximzaika@hotmail.com>
2020-10-18 21:44:45 +0800

    created new db table drought_government. Converted drought government section to php

--- php/drought-content.php
+++ php/drought-content.php
@@ -1,6 +1,6 @@
 <?php   /* This file is called by the drought.php file.
            1) Intro (the right side)
-		   2) */
+		   2) What the government does */
 
 		/* ------------- 1) Intro ------------- */		
 		$get_drought_intro = "SELECT drought_landing_id, drought_landing_title, drought_landing_subheading, drought_landing_description, drought_landing_icon, drought_landing_content FROM drought_landing";
@@ -34,6 +34,57 @@
 		
 		$drought_intro = $drought_intro . '</div></div></div>';
 		
-			
+		/* ------------- 1) What the government does ------------- */
+		$get_drought_govern = "SELECT govern_id,
+									  govern_ico,
+									  govern_title,
+									  govern_fron_descr,
+									  govern_back_descr,
+									  govern_url
+							  FROM drought_government";
+		$drought_govern_data = $con -> query($get_drought_govern);
+
+		$governmentInfo = '';
+		
+		if ($drought_govern_data->num_rows > 0) {
+			while($row = $drought_govern_data->fetch_assoc()) {
+				$govern_id = $row['govern_id'];
+				$govern_ico = $row['govern_ico'];
+				$govern_title = $row['govern_title'];
+				$govern_fron_descr = $row['govern_fron_descr'];
+				$govern_back_descr = $row['govern_back_descr'];
+				$govern_url = $row['govern_url'];
+				
+				$governmentInfo = $governmentInfo .
+					'<div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-3 col-xxxl-3 d-flex align-items-stretch pt-3">'.
+						'<div class="card card-flip h-100 w-100">'.
+							'<div class="card-front text-white">'.
+								'<div class="services text-center bg-light">'.
+									'<div class="icon d-flex justify-content-center align-items-center">'.
+										'<span class="'.$govern_ico.'"></span>'.
+									'</div>'.
+										
+									'<div class="text" style="padding-bottom:25px;">'.
+										'<h4><b>'.$govern_title.'</b></h4>'.
+										'<h5>'.$govern_fron_descr.'</h5>'.
+									'</div>'.
+									
+									'<div class="card-custom d-flex align-items-center justify-content-center">'.
+										'TAP TO VIEW'.
+									'</div>'.
+								'</div>'.
+							'</div>'.
+							
+							'<div class="card-back bg-light">'.
+								'<div class="card-body">'.
+									'<h4 class="card-title"><b>'.$govern_title.'</b></h4>'.
+									'<h5 class="card-text">'.$govern_back_descr.'</h5>'.
+									'<button onclick="'."location.href='".$govern_url."'".'"'.'  type="button" class="btn btn-topic text-center btn-amazing-techniques" style="padding-left: 30px; padding-right: 30px;">Learn more</button>'.
+								'</div>'.
+							'</div>'.
+						'</div>'.
+					'</div>';
+			}
+		}
 		
 ?>
