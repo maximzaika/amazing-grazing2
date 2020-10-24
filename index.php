cbcee8655f7b754b85eb4e6065674cb4f59aa2c1
Maxim Zaika <maximzaika@hotmail.com>
2020-10-24 22:45:08 +0800

    test ajax protection

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	<head>
 		<title>Amazing Grazing - Protecting Australian Grasslands</title>
 		<meta charset="utf-8">
-		<meta name="csrf-token" content="<?php $_SESSION['csrf_token']; ?>">
+		<meta name="csrf-token" content="<?php $_SESSION['csrf_token']; echo $_SESSION['csrf_token']; ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		
 		<!-- Browser tab logo -->
