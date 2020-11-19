d2d1b5cc8cf57d60d2627e5a7ea0d853403e51f3
Maxim Zaika <maximzaika@hotmail.com>
2020-11-20 02:23:46 +0800

    few changes to match the new AWS webser architecture

--- js/amazing-grazing/livestock-prediction.js
+++ js/amazing-grazing/livestock-prediction.js
@@ -343,9 +343,9 @@ $(function(){
 					
 					var preview_beef = data.preview_beef.split('~');
 					var modal_beef_title = data.modal_beef_title.split('~');
-					/*var modal_beef_text = data.modal_beef_text.split('~');
+					//var modal_beef_text = data.modal_beef_text.split('~');
 					var preview_dairy = data.preview_dairy.split('~');
-					var modal_dairy_title = data.modal_dairy_title.split('~');
+					/*var modal_dairy_title = data.modal_dairy_title.split('~');
 					var modal_dairy_text = data.modal_dairy_text.split('~');
 					var preview_milk = data.preview_milk.split('~');
 					var modal_milk_title = data.modal_milk_title.split('~');
